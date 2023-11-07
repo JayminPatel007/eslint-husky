014813032bedc2dd87ea8022664ec8f9c1129b93
Jaymin Patel <jaymin.future365@gmail.com>
2023-11-07 13:01:27 +0530

    Test

---
+++ src/index.js
@@ -0,0 +1,156 @@
+// // let a = 10;
+// [1, 2, 3, 4].forEach((i) => {
+//   return 3;
+//   console.log(i);
+// });
+// const a = ['test', 'test2', 'test3'];
+// const [, test2, test3] = a;
+// console.log(test2);
+// console.log(test3);
+// const testing = {
+//   test123: 'test',
+// };
+
+// const TEST_RULE = 'Test rule';
+// const test_rule = "mew test rule"
+
+// console.log(test_rule)
+
+// console.log(variable);
+// var variable;
+
+// function verify(n) {
+//   return n > 100;
+// }
+
+// function verifyList(head) {
+//   let item = head;
+//   do {
+//     if (verify(item)) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   } while (item);
+// }
+
+// const b = testing.test123;
+
+// console.log(b == testing.test123);
+
+// const foo = new Symbol('foo');
+// const bar = new BigInt(9007199254740991);
+
+// console.log(bar)
+
+// function test3(condition, DEFAULT) {
+//   return (condition ? condition : {}) || DEFAULT;
+// }
+
+// async function test2() {
+//   return new Promise(() => {
+//      return 1;
+//   });
+// }
+
+// async function test() {
+//   for (let i = 0; i < 5; i++) {
+//     await test2();
+//   }
+// }
+
+// function test(val) {
+//   switch (val) {
+//     case 1:
+//       return "small"
+//     case 2:
+//       return "little big"
+//     default:
+//       return "Test"
+//   }
+// }
+
+// function f(a = 0, b) {
+//   console.log(a);
+//   console.log(b)
+// }
+
+// function g(a, b = 0, c) {
+//   console.log(a)
+//   console.log(b)
+//   console.log(c);
+// }
+
+// test(5);
+
+// const x = 40;
+
+// if (x == 42) { }
+
+// if ("" == text) { }
+
+// if (obj.getStuff() != undefined) { }
+
+// function higherOrderFunction(num) {
+//   return num > 20;
+// }
+
+// const isMoreThan20 = higherOrderFunction;
+
+// console.log(isMoreThan20(10));
+
+// class Test {
+//   a;
+//   b;
+
+//   constructor() {
+//     this.a = 'test';
+//     this.b = 'test2';
+//   }
+// }
+
+// let obj2;
+
+// const obj = new Test();
+
+// console.log(obj);
+
+// A: for (let i of [1, 2, 3, 4, 5]) {
+//   console.log(i);
+//   if (i > 3) {
+//     break A;
+//   }
+// }
+
+// const arr = [];
+// const test = arr[3];
+// console.log(test);
+
+// function doSomething() {
+//   console.log("do something")
+// }
+
+// function doSomethingElse() {
+//   console.log('do something else');
+// }
+
+// if (arr) {
+//   doSomethingElse();
+// } else {
+//   doSomething();
+// }
+
+// const a = [1, 2, 3, 4, 5];
+
+// function filter(arr) {
+//   arr[0] = 5;
+//   console.log(arr)
+// }
+
+// filter(a);
+
+// const undefined = 5;
+//
+// let b;
+//
+console.log(undefined);
